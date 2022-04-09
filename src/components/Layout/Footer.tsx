6d650b1b1b7358f6bfa02d682309acbdb8871d6a
arippicnic <arifdwimusic@gmail.com>
2022-04-09 20:32:30 +0700

    footer flex

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -5,22 +5,21 @@ const Footer: React.FC = () => {
   const links = [
     { label: "Open Source", href: "https://github.com/arippicnic/trading-mode" },
     { label: "Credits", href: "/credit" },
+    { label: `©${new Date().getFullYear()} ${siteMetadata.author}`, href: "/" },
   ].map(({ label, href }: { label: string; href: string }) => {
     return (
       <Link href={href}>
-        <li>{label}</li>
+        <li>
+          {` • `}
+          {label}
+        </li>
       </Link>
     );
   });
 
   return (
     <footer className="flex flex-col items-center pt-20">
-      <div className="mb-3 text-sm text-gray-500 dark:text-gray-40 text-center">
-        <ul className="pb-2 flex space-x-2 0">{links}</ul>
-        <Link href="/">
-          {`© ${new Date().getFullYear()}`} {siteMetadata.author}
-        </Link>
-      </div>
+      <ul className="flex space-x-2 0 mb-3 text-sm text-gray-500 dark:text-gray-40 text-center">{links}</ul>
     </footer>
   );
 };
