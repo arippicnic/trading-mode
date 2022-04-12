396076e44a658ecd63dd4bc08a68523179e13100
arippicnic <arifdwimusic@gmail.com>
2022-04-13 05:23:52 +0700

    toltip position

--- src/components/FearGreadIndex.tsx
+++ src/components/FearGreadIndex.tsx
@@ -7,19 +7,18 @@ export interface FearType {
 }
 
 const FearGreadIndex: React.FC<FearType> = ({ fication, value }) => {
-  const toltipDatas = isToltip({ name: "Fear and Greed Index",
-   loop: false });
+  const toltipDatas = isToltip({ name: "Fear and Greed Index", loop: false });
   const color = perc2color(value);
   return (
-    <>
+    <div className="flex flex-col items-center">
       <div {...toltipDatas} className="pt-5 text-gray-500 dark:text-gray-400 flex flex-col items-center">
         <p>{fication}</p>
-        <div className="w-fit text-sm sm:text-sm md:text-md lg:text:lg bg-black rounded-md p-2 mt-1">
+        <div className="w-fit text-sm sm:text-sm md:text-md lg:text:lg bg-black rounded-md px-2 py-1 mt-1">
           <p style={{ color }}>{value}</p>
         </div>
       </div>
       <Tooltip id={toltipDatas["data-for"]} place="bottom" />
-    </>
+    </div>
   );
 };
 
