b4c22e4d748794fcdfe60475bcb86cefa35a8dcb
arippicnic <arippicnic@gmail.com>
2022-12-25 02:37:53 +0700

    package update & use yarn v2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  experimental: {
+    newNextLinkBehavior: false,
+  },
 };
 
 module.exports = nextConfig;
